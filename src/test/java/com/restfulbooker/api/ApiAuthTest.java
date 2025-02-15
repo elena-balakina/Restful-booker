ea31909449453ef1b8699ec9cd9ea748866d745c
ebalakina <elena.balakina@diabolocomc.com>
2025-02-15 20:01:52 +0700

    Add Config

--- src/test/java/com/restfulbooker/api/ApiAuthTest.java
+++ src/test/java/com/restfulbooker/api/ApiAuthTest.java
@@ -1,6 +1,7 @@
 package com.restfulbooker.api;
 
 import com.restfulbooker.api.api.AuthApi;
+import com.restfulbooker.api.config.Config;
 import com.restfulbooker.api.payloads.Auth;
 import io.restassured.response.Response;
 import org.approvaltests.Approvals;
@@ -11,13 +12,30 @@ public class ApiAuthTest {
 
     @Test
     public void authApiShouldReturnToken() {
+        String username = Config.getProperty("username");
+        String password = Config.getProperty("password");
+
         Auth auth = new Auth.Builder()
-                .setUsername("admin")
-                .setPassword("password123")
+                .setUsername(username)
+                .setPassword(password)
                 .build();
         Response response = AuthApi.postAuth(auth);
         Approvals.verify(response.getStatusCode());
         String actualToken = response.getBody().jsonPath().get("token").toString();
         Assertions.assertFalse(actualToken.isEmpty());
+    }
+
+    @Test
+    public void authApiShouldReturn400ForInvalidPassword() {
+        String username = Config.getProperty("username");
+        String password = "invalid-password";
+
+        Auth auth = new Auth.Builder()
+                .setUsername(username)
+                .setPassword(password)
+                .build();
+        Response response = AuthApi.postAuth(auth);
+        String reasonInResponse = response.getBody().jsonPath().get("reason").toString();
+        Assertions.assertEquals("Bad credentials", reasonInResponse);
     }
 }
